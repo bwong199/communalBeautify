21e644dd2ae3b2e2caf4a0490d35f24efd102fca
Ben Wong <bwong199@my.bcit.ca>
2015-12-03 14:41:52 -0700

    First Commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.criminalintent;
+
+/**
+ * Created by benwong on 2015-12-02.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String SUSPECT = "suspect";
+        }
+    }
+}
